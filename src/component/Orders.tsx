b5e77bb185561e72c0cccf483e9e294275daa459
ShaDow326-del <iruhchidi@gmail.com>
2026-01-15 18:18:30 +0100

    Add Orders component to display order information

---
+++ src/component/Orders.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Orders: React.FC = () => {
+  return (
+    <div className="bg-[#1e2329] rounded-xl p-6">
+      <h2 className="text-xl font-bold mb-4">All Orders</h2>
+      <p>Orders will appear in Spot or Futures sections as you trade.</p>
+    </div>
+  );
+};
+
+export default Orders;
